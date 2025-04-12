f8e8173752cb8f58a00b42f4f966ffc39f8ca07c
UserMaster <127202577+thucnguyen12356666@users.noreply.github.com>
2025-04-12 22:56:21 +0700

    Update Program.cs

--- SWP391_SkinCare/Program.cs
+++ SWP391_SkinCare/Program.cs
@@ -20,7 +20,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseRouting();
 app.MapControllers();
 
-app.Run();
+app.Run("http://0.0.0.0:8501");
